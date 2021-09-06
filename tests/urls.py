f1a101332dbc81ade483ae9a19f69a207442350f
Rem <rem.kudusov@gmail.com>
2021-09-06 15:44:41 +0300

    :art: updated urlpatterns

--- tests/urls.py
+++ tests/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('sign_up/', sign_up_view.as_view()),
     path('log_in/', log_in_view.as_view()),
     path('log_out/', log_out_view.as_view()),
-    path('get/tests/', tests_view.as_view()),
-    path('post/answers/', answer_view.as_view()),
+    path('tests/', tests_view.as_view()),
+    path('answers/', answer_view.as_view()),
 ]
