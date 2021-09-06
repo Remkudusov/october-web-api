5cf039304028a8fbe6e253aaec7b67b497a26845
Rem <rem.kudusov@gmail.com>
2021-09-06 15:47:54 +0300

    :art: updated link for answer

--- tests/urls.py
+++ tests/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('log_in/', log_in_view.as_view()),
     path('log_out/', log_out_view.as_view()),
     path('tests/', tests_view.as_view()),
-    path('answers/', answer_view.as_view()),
+    path('answer/', answer_view.as_view()),
 ]
